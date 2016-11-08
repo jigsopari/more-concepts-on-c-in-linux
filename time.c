ce3a534fe171bb8851e4b747c133921004602529
jigyasaPopat <jigyasapopat@raveendra-OptiPlex-9020.(none)>
2016-11-08 14:15:34 +0530

    use of time structures

--- time.c
+++ time.c
@@ -9,5 +9,11 @@ struct tm *mytm;
 mytime=time(NULL);
 printf("%s\n",ctime(&mytime));
 
+mytm=localtime(&mytime);
+printf("year=%d\n",1900+mytm->tm_year);
+printf("month=%d\n",1+mytm->tm_mon);
+printf("day of the month:%d\n",mytm->tm_mday);
+printf("DST:%d\n",mytm->tm_isdst);
+
 }
 

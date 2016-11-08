ec1acd3a1ed712e4e8623a461cc207ba188f886a
jigsopari <popat.jigyasa@gmail.com>
2016-11-08 11:41:35 +0530

    Update new_static.c

--- new_static.c
+++ new_static.c
@@ -17,3 +17,7 @@ void main()
  myfunc();
  
 }
+--> notes: when i is declared as static variable it will preserver the value of i even when the function call has finished whereas if it is 
+not declared as static it will over write values of i and results will be i=2, i=4; i-2,i=4.
+ 
+ and in the static case the results are:-> i=2,i-4;i=6;i=8.

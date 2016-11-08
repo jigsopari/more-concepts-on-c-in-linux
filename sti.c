e4de329cb312b0cc927d12b2ed9aace893d1060c
jigyasaPopat <jigyasapopat@raveendra-OptiPlex-9020.(none)>
2016-11-08 11:59:35 +0530

    string to integer

---
+++ sti.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+
+char *stri="12345";
+char *strf="1234.5678";
+
+int i=atoi(stri);
+double f=atof(strf);
+
+printf("interger = %d\n",i);
+printf("float= %f \n",f);
+
+}

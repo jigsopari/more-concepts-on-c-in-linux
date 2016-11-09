d69d91310156cce5a6d40267eed95b94c83083b0
jigyasaPopat <jigyasapopat@raveendra-OptiPlex-9020.(none)>
2016-11-09 11:16:28 +0530

    enumeration strategy

---
+++ enum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+typedef enum day { Monday=1, Tuesday, wednesday,thursday, friday, saturday, sunday }sched;
+void main()
+{
+
+sched myday=wednesday;
+printf("Monday is=%d\n",Monday);
+printf("Tuesday is=%d\n",Tuesday);
+printf("wednesday is=%d\n",wednesday);
+printf("thursday is=%d\n",thursday);
+printf("friday is=%d\n",friday);
+printf("saturday is=%d\n",saturday);
+printf("sunday is=%d\n",sunday);
+
+printf("Myday is=%d\n",myday);
+}
+
